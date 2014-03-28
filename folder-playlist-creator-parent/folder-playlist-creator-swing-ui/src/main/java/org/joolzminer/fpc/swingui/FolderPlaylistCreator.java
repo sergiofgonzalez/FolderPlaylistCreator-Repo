6cd327c84e279fca462a7a8988003275b8719df1
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-28 19:55:14 +0100

    Added menu to the FolderPlaylistCreator.

--- folder-playlist-creator-parent/folder-playlist-creator-swing-ui/src/main/java/org/joolzminer/fpc/swingui/FolderPlaylistCreator.java
+++ folder-playlist-creator-parent/folder-playlist-creator-swing-ui/src/main/java/org/joolzminer/fpc/swingui/FolderPlaylistCreator.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,6 +16,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -42,10 +47,77 @@ public class FolderPlaylistCreator extends JFrame {
 			"[no folder selected]");
 	private JButton createButton = new JButton("Create");
 	private JScrollPane scrollPane;
+	private JMenuItem fileCreateMenuItem;
 
 	private DirFilesReader dirFilesReader;
 	private PlaylistWriter playlistWriter;
 	private Collection<Path> playlistFiles;
+
+	private class SelectFolderAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			fileChooser.setDialogTitle("Select Folder");
+			int returnValue = fileChooser.showOpenDialog(null);
+			if (returnValue == JFileChooser.APPROVE_OPTION) {
+				selectedFolderTextField.setText(fileChooser.getSelectedFile()
+						.getAbsolutePath());
+
+				try {
+					playlistFiles = dirFilesReader.read(Paths
+							.get(selectedFolderTextField.getText()));
+				} catch (IOException ex) {
+					JOptionPane.showMessageDialog(null,
+							"Could not read selected folder", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				if (playlistFiles.isEmpty()) {
+					JOptionPane.showMessageDialog(null,
+							"Selected folder is Empty", "Warning",
+							JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				folderContentsTextArea.setText(null);
+				for (Path fileInFolder : playlistFiles) {
+					folderContentsTextArea.setText(folderContentsTextArea
+							.getText() + fileInFolder + "\n");
+				}
+
+				createButton.setEnabled(true);
+				fileCreateMenuItem.setEnabled(true);
+				revalidate();
+			}
+		}
+	}
+
+	private class CreatePlaylistAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Path playlistFilename = Paths.get(selectedFolderTextField.getText()
+					+ ".m3u");
+			LOGGER.debug("playlistFilename={}", playlistFilename);
+			if (Files.exists(playlistFilename)) {
+				Path onlyFilename = Paths.get(playlistFilename.toString())
+						.getFileName();
+				int response = JOptionPane.showConfirmDialog(null, onlyFilename
+						+ "\nalready exists.\n Do you want to replace it?",
+						"Confirm Overwrite", JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE);
+				if (response == JOptionPane.NO_OPTION) {
+					return;
+				}
+			}
+			playlistWriter.write(playlistFilename, playlistFiles);
+			JOptionPane.showMessageDialog(null,
+					Paths.get(playlistFilename.toString()).getFileName()
+							+ " created.", "Information",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
 
 	public FolderPlaylistCreator(String title) {
 		super(title);
@@ -65,44 +137,7 @@ public class FolderPlaylistCreator extends JFrame {
 
 		selectFolderButton.setIcon(new ImageIcon(
 				"target/classes/icons/audio-folder-icon-32x32.png"));
-		selectFolderButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser fileChooser = new JFileChooser();
-				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				fileChooser.setDialogTitle("Select Folder");
-				int returnValue = fileChooser.showOpenDialog(null);
-				if (returnValue == JFileChooser.APPROVE_OPTION) {
-					selectedFolderTextField.setText(fileChooser
-							.getSelectedFile().getAbsolutePath());
-
-					try {
-						playlistFiles = dirFilesReader.read(Paths
-								.get(selectedFolderTextField.getText()));
-					} catch (IOException ex) {
-						JOptionPane.showMessageDialog(null,
-								"Could not read selected folder", "Error",
-								JOptionPane.ERROR_MESSAGE);
-						return;
-					}
-					if (playlistFiles.isEmpty()) {
-						JOptionPane.showMessageDialog(null,
-								"Selected folder is Empty", "Warning",
-								JOptionPane.WARNING_MESSAGE);
-						return;
-					}
-					folderContentsTextArea.setText(null);
-					for (Path fileInFolder : playlistFiles) {
-						folderContentsTextArea.setText(folderContentsTextArea
-								.getText() + fileInFolder + "\n");
-					}
-
-					createButton.setEnabled(true);
-					revalidate();
-				}
-			}
-		});
+		selectFolderButton.addActionListener(new SelectFolderAction());
 
 		selectedFolderTextField.setEditable(false);
 		selectedFolderTextField.setPreferredSize(new Dimension(400, 25));
@@ -115,31 +150,7 @@ public class FolderPlaylistCreator extends JFrame {
 
 		createButton.setEnabled(false);
 
-		createButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Path playlistFilename = Paths.get(selectedFolderTextField
-						.getText() + ".m3u");
-				LOGGER.debug("playlistFilename={}", playlistFilename);
-				if (Files.exists(playlistFilename)) {
-					Path onlyFilename = Paths.get(playlistFilename.toString()).getFileName();
-					int response = JOptionPane
-										.showConfirmDialog(null,
-												onlyFilename + "\nalready exists.\n Do you want to replace it?",
-									"Confirm Overwrite",
-									JOptionPane.YES_NO_OPTION,
-									JOptionPane.QUESTION_MESSAGE);
-					if (response == JOptionPane.NO_OPTION) {
-						return;
-					}
-				}
-				playlistWriter.write(playlistFilename, playlistFiles);
-				JOptionPane.showMessageDialog(null, 
-						Paths.get(playlistFilename.toString()).getFileName() + " created.",
-						"Information", JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
+		createButton.addActionListener(new CreatePlaylistAction());
 
 		// adding all components
 		add(selectFolderButton);
@@ -147,6 +158,72 @@ public class FolderPlaylistCreator extends JFrame {
 		add(scrollPane);
 		add(createButton);
 
+		initMenu();
+	}
+
+	private void initMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu helpMenu = new JMenu("Help");
+
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+
+		JMenuItem fileNewMenuItem = new JMenuItem("New");
+		JMenuItem fileOpenMenuItem = new JMenuItem("Open...", new ImageIcon(
+				"target/classes/icons/audio-folder-icon-16x16.png"));
+		fileOpenMenuItem.addActionListener(new SelectFolderAction());
+		fileCreateMenuItem = new JMenuItem("Create");
+		fileCreateMenuItem.setEnabled(false);
+		fileCreateMenuItem.addActionListener(new CreatePlaylistAction());
+		JMenuItem fileExitMenuItem = new JMenuItem("Exit");
+		fileExitMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				confirmExitApp();
+			}
+		});
+
+		fileNewMenuItem.setEnabled(false);
+		fileMenu.add(fileNewMenuItem);
+		fileMenu.add(fileOpenMenuItem);
+		fileMenu.add(fileCreateMenuItem);
+		fileMenu.addSeparator();
+		fileMenu.add(fileExitMenuItem);
+
+		JMenuItem cutMenuItem = new JMenuItem("Cut");
+		cutMenuItem.setEnabled(false);
+		JMenuItem copyMenuItem = new JMenuItem("Copy");
+		copyMenuItem.setEnabled(false);
+		JMenuItem pasteMenuItem = new JMenuItem("Paste");
+		pasteMenuItem.setEnabled(false);
+
+		editMenu.add(cutMenuItem);
+		editMenu.add(copyMenuItem);
+		editMenu.add(pasteMenuItem);
+
+		JMenuItem aboutMenuItem = new JMenuItem(
+				"About FolderPlaylistCreator...");
+		helpMenu.add(aboutMenuItem);
+		aboutMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"FolderPlaylistCreator v1.0-SNAPSHOT",
+								"About FolderPlaylistCreator",
+								JOptionPane.OK_OPTION,
+								new ImageIcon(
+										"target/classes/icons/audio-folder-icon-128x128.png"));
+			}
+		});
+
+		setJMenuBar(menuBar);
 	}
 
 	private static void constructGUI() {
@@ -156,7 +233,25 @@ public class FolderPlaylistCreator extends JFrame {
 		frame.setSize(575, 660);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
+		frame.setIconImage(new ImageIcon(
+				"target/classes/icons/audio-folder-icon-32x32.png").getImage());
+		frame.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				confirmExitApp();
+			}
+		});
 		frame.setVisible(true);
+	}
+
+	public static void confirmExitApp() {
+		int userOption = JOptionPane.showConfirmDialog(null,
+				"Exit FolderPlaylistCreator?", "Exit Application",
+				JOptionPane.YES_NO_OPTION);
+		if (userOption == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
 	}
 
 	public static void setupGlobalExceptionHandling() {
