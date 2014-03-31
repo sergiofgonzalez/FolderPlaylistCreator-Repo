79ad961642619ae32b1946fcc4f53a325d007468
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-31 13:49:17 +0200

    Removed unused imports.

--- folder-playlist-creator-parent/folder-playlist-creator-services/src/main/java/org/joolzminer/fpc/service/PlaylistWriterImpl.java
+++ folder-playlist-creator-parent/folder-playlist-creator-services/src/main/java/org/joolzminer/fpc/service/PlaylistWriterImpl.java
@@ -6,7 +6,6 @@ import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 
